9a379854f4059152373525a5f35e78cae0f9b7ed
Arnaud <a.gay@fondamentalgroup.com>
2024-05-07 16:48:24 +0200

    [maj] correction

--- easy_attribut_code/models/product_product.py
+++ easy_attribut_code/models/product_product.py
@@ -9,43 +9,27 @@ class ProductProduct(models.Model):
                                                           domain=[('attribute_line_id.value_count', '>', 0)],
                                                           string="Variant Values", ondelete='restrict')
     fg_ref = fields.Char(compute='_compute_myref')
+    #default_code = fields.Char('Internal Reference', compute='_compute_myref', store= True)
 
     @api.depends('product_template_variant_value_ids', 'code')
     def _compute_myref(self):
         for record in self:
+            i = 0
             attributs = record.product_template_variant_value_ids
             _logger.info("ATTRIBUTS : " + str(attributs))
             res = ""
             for attribut in attributs:
                 _logger.info(" CODE ::: " + str(attribut.product_attribute_value_id.code))
                 res = res + str(attribut.product_attribute_value_id.code)
-            record.fg_ref = self.product_tmpl_id.code + res
+                i = i + 1
+                if i == 1:
+                    res += self.product_tmpl_id.code
+            record.fg_ref = res
+            record.default_code = res
 
 
 
-    @api.model_create_multi
-    def create(self, vals_list):
-        #_logger.info("------ CREATE Product_product CA PASSE ICI  -----")
-        #_logger.info(type(vals_list))
-        #_logger.info(vals_list)
-        for val_list in vals_list:
-            variants = val_list["product_template_attribute_value_ids"][0]
-            #_logger.info(len(variants))
-            #_logger.info(variants)
-            vars = variants[2]
-            #_logger.info(vars)
-            code_pdt = self.env['product.template'].search([('id', '=', val_list["product_tmpl_id"])]).code
-            #_logger.info(code_pdt)
-            res = code_pdt
-            for var in vars:
-                code = self.env['product.template.attribute.value'].search([('id', '=', var)]).product_attribute_value_id.code
-                res = res + code
-                #_logger.info(code)
-            val_list["default_code"] = res
-        # _logger.info(vals_list)
-        products = super(ProductProduct, self.with_context(create_product_product=True)).create(vals_list)
-        self.env.registry.clear_cache()
-        return products
+
 
     #
     #

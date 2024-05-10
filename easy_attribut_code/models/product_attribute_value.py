8f1dd0038a6c636b3d64a6c02feb59c9c218edf0
Arnaud <a.gay@fondamentalgroup.com>
2024-05-10 12:23:56 +0200

    [add] integrate tracking on attribute

--- easy_attribut_code/models/product_attribute_value.py
+++ easy_attribut_code/models/product_attribute_value.py
@@ -2,6 +2,15 @@ from odoo import fields, models, api
 import logging
 _logger = logging.getLogger(__name__)
 
+
 class ProductAttributeValue(models.Model):
-    _inherit = 'product.attribute.value'
-    code = fields.Text('Code', required=True)
+    _name = 'product.attribute.value'
+    code = fields.Text('Code', required=True,)
+
+class ProductAttributeValue(models.Model):
+    _inherit = ['product.attribute', 'mail.thread', 'mail.activity.mixin']
+    _name = 'product.attribute'
+    value_ids = fields.One2many(
+        comodel_name='product.attribute.value',
+        inverse_name='attribute_id',
+        string="Values", copy=True, tracking=True,)
